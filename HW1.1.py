4dfe14a50c3b89e8a9e81257177e1ec9f334bef0
AkaVelial <125206237+AkaVelial@users.noreply.github.com>
2023-02-15 00:42:37 +0200

    Update HW1.1.py

--- HW1.1.py
+++ HW1.1.py
@@ -1,3 +1,3 @@
-first_Number = int(input("Введите первое число:"))  # first val
-second_Number = int(input("Введите второе число:"))  # second val
+first_Number = float(input("Введите первое число:"))  # first val
+second_Number = float(input("Введите второе число:"))  # second val
 print(first_Number + second_Number)  # sum operation between values
