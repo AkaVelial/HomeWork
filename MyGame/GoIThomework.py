9247b57ec5a0103566698b0f34eb1a0f0e90fd7e
AkaVelial <125206237+AkaVelial@users.noreply.github.com>
2023-02-16 21:28:38 +0200

    Update GoIThomework.py

--- MyGame/GoIThomework.py
+++ MyGame/GoIThomework.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame. constants import QUIT
+from pygame.constants import QUIT
 
 pygame.init()
 
@@ -13,7 +13,7 @@ RED = 255, 0, 0
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
-ball.fill((WHITE))
+ball.fill(WHITE)
 ball_rect = ball.get_rect()
 ball_speed = [1, 1]
 
@@ -28,21 +28,21 @@ while is_working:
 
     if ball_rect.bottom >= heigth:
         ball_speed[1] = -ball_speed[1]
-        ball.fill((BLUE))
+        ball.fill(BLUE)
 
     elif ball_rect.top <= 0:
         ball_speed[1] = -ball_speed[1]
-        ball.fill((GREEN))
+        ball.fill(GREEN)
 
     elif ball_rect.right >= width:
         ball_speed[0] = -ball_speed[0]
-        ball.fill((RED))
+        ball.fill(RED)
 
     elif ball_rect.left <0:
         ball_speed[0] = -ball_speed[0]
-        ball.fill((WHITE))
+        ball.fill(WHITE)
 
-    main_surface.fill((BLACK))
+    main_surface.fill(BLACK)
 
     main_surface.blit(ball, ball_rect)
     pygame.display.flip()
